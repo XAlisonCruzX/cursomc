c105a07132c338ac14b3bc5118a398058c61ab18
XAlisonCruzX <alisonofdark@gmail.com>
2021-05-20 05:54:00 -0300

    Atualizando Categoria com Put, outros pequenos ajustes

--- src/main/java/com/alisoncruz/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/alisoncruz/cursomc/resources/CategoriaResource.java
@@ -22,7 +22,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 
 		Categoria obj = service.find(id);
 
@@ -30,12 +30,22 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Object> insert(@RequestBody Categoria obj){
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 		
 	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	
 
 }
